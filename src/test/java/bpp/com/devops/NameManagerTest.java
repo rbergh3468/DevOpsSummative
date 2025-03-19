8815093d7e8648b46af05e1550bd4e17418e4e95
rbergh3468 <59732619+rbergh3468@users.noreply.github.com>
2025-03-19 22:09:46 +0000

    Update NameManagerTest.java
    Added missing unit tests under name manager class

--- src/test/java/bpp/com/devops/NameManagerTest.java
+++ src/test/java/bpp/com/devops/NameManagerTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+
 class NameManagerTest {
 
     private NameManager testObject;
@@ -22,6 +24,22 @@ class NameManagerTest {
         assertNotNull(name, "Name should not be null");
         assertEquals("Secret Squirrel", name, "Name does not match");
 
+    }
+    
+    @Test
+    public void testAddAnimal() { 	
+    	
+    	testObject.addAnimal("Horse");
+    	
+        assertEquals("Secret Squirrel", "Secret Horse", "Name does not match");
+    }
+    
+    @Test
+    public void testAddAdjective() { 	
+    	
+    	testObject.addAdjective("Golden");
+    	
+        assertEquals("Secret Squirrel", "Goldent Horse", "Name does not match");
     }
 
 }
